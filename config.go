3104c0de0fe55e3d2194b8f8f3747eb5c6ff8415
Anjum Malik <47303481+anjumm@users.noreply.github.com>
2024-10-08 14:48:43 +0200

    Update config.go

--- config.go
+++ config.go
@@ -310,18 +310,18 @@ func DefaultLANConfig() *Config {
 		ProtocolVersion:         ProtocolVersion2Compatible,
 		TCPTimeout:              10 * time.Second,       // Timeout after 10 seconds
 		IndirectChecks:          3,                      // Use 3 nodes for the indirect ping
-		RetransmitMult:          4,                      // Retransmit a message 4 * log(N+1) nodes
-		SuspicionMult:           4,                      // Suspect a node for 4 * log(N+1) * Interval
+		RetransmitMult:          1,                      // Retransmit a message 4 * log(N+1) nodes
+		SuspicionMult:           1,                      // Suspect a node for 4 * log(N+1) * Interval
 		SuspicionMaxTimeoutMult: 6,                      // For 10k nodes this will give a max timeout of 120 seconds
-		PushPullInterval:        30 * time.Second,       // Low frequency
-		ProbeTimeout:            500 * time.Millisecond, // Reasonable RTT time for LAN
-		ProbeInterval:           1 * time.Second,        // Failure check every second
+		PushPullInterval:        150 * time.Second,       // Low frequency
+		ProbeTimeout:            1 * time.Second, // Reasonable RTT time for LAN
+		ProbeInterval:           120 * time.Second,        // Failure check every second
 		DisableTcpPings:         false,                  // TCP pings are safe, even with mixed versions
-		AwarenessMaxMultiplier:  8,                      // Probe interval backs off to 8 seconds
+		AwarenessMaxMultiplier:  6,                      // Probe interval backs off to 8 seconds
 
-		GossipNodes:          2,                      // Gossip to 3 nodes
-		GossipInterval:       8000 * time.Millisecond, // Gossip more rapidly
-		GossipToTheDeadTime:  30 * time.Second,       // Same as push/pull
+		GossipNodes:          1,                      // Gossip to 3 nodes
+		GossipInterval:       15 * time.Second, // Gossip more rapidly
+		GossipToTheDeadTime:  150 * time.Second,       // Same as push/pull
 		GossipVerifyIncoming: true,
 		GossipVerifyOutgoing: true,
 
