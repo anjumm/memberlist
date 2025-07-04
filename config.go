996373e5b93224e902e273d114eee771f373e3d2
Anjum Malik <47303481+anjumm@users.noreply.github.com>
2025-07-04 09:44:20 +0200

    Update config.go

--- config.go
+++ config.go
@@ -309,18 +309,18 @@ func DefaultLANConfig() *Config {
 		AdvertisePort:           7946,
 		ProtocolVersion:         ProtocolVersion2Compatible,
 		TCPTimeout:              15 * time.Second,       // Timeout after 10 seconds
-		IndirectChecks:          1,                      // Use 3 nodes for the indirect ping
+		IndirectChecks:          3,                      // Use 3 nodes for the indirect ping
 		RetransmitMult:          1,                      // Retransmit a message 4 * log(N+1) nodes
 		SuspicionMult:           1,                      // Suspect a node for 4 * log(N+1) * Interval
 		SuspicionMaxTimeoutMult: 6,                      // For 10k nodes this will give a max timeout of 120 seconds
-		PushPullInterval:        150 * time.Second,       // Low frequency
-		ProbeTimeout:            2 * time.Second, // Reasonable RTT time for LAN
-		ProbeInterval:           120 * time.Second,        // Failure check every second
+		PushPullInterval:        30 * time.Second,       // Low frequency
+		ProbeTimeout:            200 * time.Millisecond, // Reasonable RTT time for LAN
+		ProbeInterval:           15 * time.Second,        // Failure check every second
 		DisableTcpPings:         false,                  // TCP pings are safe, even with mixed versions
-		AwarenessMaxMultiplier:  10,                      // Probe interval backs off to 8 seconds
+		AwarenessMaxMultiplier:  5,                      // Probe interval backs off to 8 seconds
 
 		GossipNodes:          1,                      // Gossip to 3 nodes
-		GossipInterval:       15 * time.Second, // Gossip more rapidly
+		GossipInterval:       200 * time.Millisecond, // Gossip more rapidly
 		GossipToTheDeadTime:  150 * time.Second,       // Same as push/pull
 		GossipVerifyIncoming: true,
 		GossipVerifyOutgoing: true,
